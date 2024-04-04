ec836c02ef934df20d7f97c496b0534f1882dcca
Aslan <urusov_1@icloud.com>
2024-04-05 03:35:04 +0400

    javaneto1 chainged

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+ System.out.println("Кол-во начисленных миль за купленный билет:");
+ short price = 26100;
+ byte rublesformile = 20;
+ int miles = price / rublesformile;
 
+                 System.out.println("Стоимость билета = " + price);
+                 System.out.println("Кол-во полученных миль = " + miles);
     }
 }
